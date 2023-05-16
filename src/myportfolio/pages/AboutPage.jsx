ae722cc36bba273384e469036efddd21919b59e6
boaltamirano <brayanaltamiranoperez@gmail.com>
2023-05-15 21:13:15 -0500

    feat: Initial content of the pages

--- src/myportfolio/pages/AboutPage.jsx
+++ src/myportfolio/pages/AboutPage.jsx
@@ -2,7 +2,19 @@
 export const AboutPage = () => {
     return (
         <div>
-            AboutPage
+            <h1>Hi, I'm Omar</h1>
+            <p>
+                Hi! I'm a Fullstack developer with experience in creating scalable and high quality web applications
+            </p>
+
+            <h1>Everview</h1>
+            <p>
+                Hi! 👋 I'm a Fullstack developer with experience in creating scalable and high quality web applications. 💻🌐 I have solid skills in programming languages like JavaScript, HTML, CSS and frameworks like React, Node.js, Express and MongoDB. I also have experience in implementing RESTful APIs and mobile app development with React Native. 📱🚀
+
+                I enjoy working in a team and collaborating on challenging projects. I'm passionate about technology and I'm always looking for new ways to improve my skills and stay updated on the latest development trends and tools. 💡💻 In addition, I focus on agile development and continuous integration to ensure that the code I produce is of high quality and ready for production release. 🚀🔍
+
+                If you are looking for a Fullstack developer with solid skills in the latest technologies, feel free to contact me!
+            </p>
         </div>
     )
 }
--- src/myportfolio/pages/TechnologyPage.jsx
+++ src/myportfolio/pages/TechnologyPage.jsx
@@ -2,7 +2,74 @@
 export const TechnologyPage = () => {
     return (
         <div>
-            TechnologyPage
+            <h2>Frontend</h2>
+            CSS3
+            HTML5
+            Bootstrap
+            React Native
+
+
+            <h2>Backend</h2>
+            Symfony
+            Laravel
+            NodeJS
+
+
+            <h2>Libraries</h2>
+            React
+            Socket.io
+            React Router
+Redux
+
+            <h2>Database</h2>
+            MongoDB
+            Redis
+            Postgres
+            MySQL
+
+
+            <h2>Lenguajes de programacion</h2>
+            JavaScript
+            PHP
+            Python
+            TypeScript
+            C
+            C++
+            Go
+            Java
+
+
+            <h2>Herramientas</h2>
+            Expo
+            Express.js
+            Insomnia
+            Docker
+            ElasticSearch
+            Jira
+            Postman
+            Kubernetes
+
+
+            <h2>cloud computing</h2>
+            AWS
+            Heroku
+            DigitalOcean
+            Google Cloud
+            Shell Script
+            Jenkins
+            <h2>Administrador de paquetes</h2>
+            NPM
+            YARN
+
+            <h2>Sistemas operativos</h2>
+            Linux,
+            Windows
+
+            <h2>Estandares/Plataformas</h2>
+            MUI
+            JWT
+            NumPy
+            
         </div>
     )
 }
