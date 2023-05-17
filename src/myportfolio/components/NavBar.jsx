7b3f397d35dc1c6aa860213faf425eb7dfe7806f
boaltamirano <brayanaltamiranoperez@gmail.com>
2023-05-16 22:04:44 -0500

    feat: start view for about

--- src/myportfolio/components/NavBar.jsx
+++ src/myportfolio/components/NavBar.jsx
@@ -1,55 +1,54 @@
 import { Link, NavLink } from "react-router-dom"
+import "../styles/navbar.css"
 
 export const NavBar = () => {
 
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+        <nav className="navbar navbar-expand-sm header-p">
 
             <Link
-                className="navbar-brand"
+                className="logo"
                 to="/"
             >
                 Omar
             </Link>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-                <ul className="navbar-nav ml-auto">
+            <ul className="navbar-nav ml-auto ul-p">
                 <NavLink
-                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                        to="/about"
-                    >
-                        About
-                    </NavLink>
+                    className={({ isActive }) => `nav-item nav-link navlink-p ${isActive ? 'active' : ''}`}
+                    to="/about"
+                >
+                    About
+                </NavLink>
 
-                    <NavLink
-                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                        to="/technology"
-                    >
-                        Technology
-                    </NavLink>
+                <NavLink
+                    className={({ isActive }) => `nav-item nav-link navlink-p ${isActive ? 'active' : ''}`}
+                    to="/technology"
+                >
+                    Technology
+                </NavLink>
 
-                    <NavLink
-                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                        to="/experience"
-                    >
-                        Experience
-                    </NavLink>
+                <NavLink
+                    className={({ isActive }) => `nav-item nav-link navlink-p ${isActive ? 'active' : ''}`}
+                    to="/experience"
+                >
+                    Experience
+                </NavLink>
 
-                    <NavLink
-                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                        to="/project"
-                    >
-                        Project
-                    </NavLink>
+                <NavLink
+                    className={({ isActive }) => `nav-item nav-link  navlink-p ${isActive ? 'active' : ''}`}
+                    to="/project"
+                >
+                    Project
+                </NavLink>
 
-                    <NavLink
-                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                        to="/contact"
-                    >
-                        Contact
-                    </NavLink>
-                </ul>
-            </div>
+                <NavLink
+                    className={({ isActive }) => `nav-item nav-link navlink-p ${isActive ? 'active' : ''}`}
+                    to="/contact"
+                >
+                    Contact
+                </NavLink>
+            </ul>
         </nav>
     )
 }
--- src/myportfolio/pages/AboutPage.jsx
+++ src/myportfolio/pages/AboutPage.jsx
@@ -1,20 +1,58 @@
+import { useEffect, useRef, useState } from 'react';
+import '../styles/about.css';
+import '../styles/contact.scss';
 
 export const AboutPage = () => {
+
+    const [sumary, setSumary] = useState(false);
+    const sumaryRef = useRef(null);
+
+    useEffect(() => {
+        if (sumary && sumaryRef.current) {
+          sumaryRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, [sumary]);
+
+    const onViewSumary = () => {
+        setSumary(!sumary)
+    }
+
     return (
-        <div>
-            <h1>Hi, I'm Omar</h1>
-            <p>
-                Hi! I'm a Fullstack developer with experience in creating scalable and high quality web applications
-            </p>
+        <div className="container">
+            <div className="row info">
+                <div className="col">
+                    <div className="content">
+                        <h2>Hi, I'm Omar</h2>
+                        <p> Hi! I'm a Fullstack developer with experience in creating scalable and high quality web applications</p>
+                        <button 
+                            onClick={ onViewSumary }
+                        >
+                            Read more
+                        </button>
+                    </div>
 
-            <h1>Everview</h1>
-            <p>
-                Hi! 👋 I'm a Fullstack developer with experience in creating scalable and high quality web applications. 💻🌐 I have solid skills in programming languages like JavaScript, HTML, CSS and frameworks like React, Node.js, Express and MongoDB. I also have experience in implementing RESTful APIs and mobile app development with React Native. 📱🚀
+                </div>
+                <div className="col imgBx">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPUMiaDR4gWujVpLgB1yKuNXFPnDNaaZUUDA&usqp=CAU" />
+                </div>
 
-                I enjoy working in a team and collaborating on challenging projects. I'm passionate about technology and I'm always looking for new ways to improve my skills and stay updated on the latest development trends and tools. 💡💻 In addition, I focus on agile development and continuous integration to ensure that the code I produce is of high quality and ready for production release. 🚀🔍
+            </div>
 
-                If you are looking for a Fullstack developer with solid skills in the latest technologies, feel free to contact me!
-            </p>
+            {
+                sumary && 
+                <div ref={sumaryRef} className='row sumary'>
+                    <h2>Get to Know me</h2>
+                    <p>
+                        Hi! 👋 I'm a Fullstack developer with experience in creating scalable and high quality web applications. 💻🌐 I have solid skills in programming languages like JavaScript, HTML, CSS and frameworks like React, Node.js, Express and MongoDB. I also have experience in implementing RESTful APIs and mobile app development with React Native. 📱🚀
+                    </p>
+                    <p>
+                        I enjoy working in a team and collaborating on challenging projects. I'm passionate about technology and I'm always looking for new ways to improve my skills and stay updated on the latest development trends and tools. 💡💻 In addition, I focus on agile development and continuous integration to ensure that the code I produce is of high quality and ready for production release. 🚀🔍
+                    </p>
+                    <p>
+                        If you are looking for a Fullstack developer with solid skills in the latest technologies, feel free to contact me!
+                    </p>
+                </div>
+            }
         </div>
     )
 }
